0df11fd6e8e9f8356099b6d2f060ec9f088c9aa0
Senitiki Rokocakau <senitiki.rokocakau@gmail.com>
2023-04-02 18:32:30 +1200

    Change the sites' UI/UX and functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -10,6 +11,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/images', to: 'images' },
+        { from: './favicon_io', to: 'favicon_io' },
+      ],
     }),
   ],
   output: {
